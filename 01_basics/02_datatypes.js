84682a6c946aeeaf696ec2bf9158fee4b5c9a7da
Ujjwal gola <146432994+Golaji01@users.noreply.github.com>
2025-06-07 06:18:24 +0000

    container config done

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -4,7 +4,26 @@
                     it works in browser or console.
 alert("3+3")
 */
-//code readibility sshoul be high
+//code readibility should be high
 console.log(3+3);
 
 console.log("Hitesh")
+
+let name="ujjwal"
+let age=18
+let isLoggedIn=false
+
+//number => 2 topower 53
+//bigint
+//string=> ""
+//boolean =>true/false,yes/no
+//null =>standalone value
+//undefined =>variable declare but value not given
+//symbol =>unique
+
+
+//object
+
+console.log(typeof "Ujjwal",age);
+console.log(typeof undefined);  //undefined
+

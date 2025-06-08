7d5f6e640edd0b5cb848cf46ccca5f5fbfb8f6d9
Ujjwal gola <146432994+Golaji01@users.noreply.github.com>
2025-06-08 04:37:55 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,29 @@
+let score=33
+let age ="35 "
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+//console.log(typeof age);
+//console.log(typeof(age));
+
+let valueInNumber =Number(age)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+//"33" => 33
+//"33abc "=>NaN
+//true =>1;false=>0
+
+let isLoggedIn =1
+let booleanIsLoggedIn =Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+//1 =>true ; 0=> false
+//"" =>false
+//"ujjwal" =>true
+
+
+let someNumber = 388
+let stringNumber = String(someNumber)
+console.log(  stringNumber);

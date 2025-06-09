802aede4d499b165a05df23850fe468bf2cc58c7
Ujjwal gola <146432994+Golaji01@users.noreply.github.com>
2025-06-09 04:47:44 +0000

    datatypes summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,33 @@
+// PRIMITIVE
+
+// 7 types:String ,Number ,Boolean,null ,Unddefined ,Symbol,BigInt
+
+const score =100
+constscoreValue =100.3;
+
+const isLoggedIn =false
+const outsideTemp = null
+let userName;
+
+const id=Symbol("123")
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+
+//REFERENCE OR NON-PRIMITIVE
+
+//Array ,Objects ,Functions
+
+const heros =["shaktiman","ironman", "doga"];
+
+let myObj={
+    name :"ujjwal",
+    age :21,
+}
+
+const myFunction=function(){
+    console.log("Hello World");
+}
+
+ /// ecma documentation operator

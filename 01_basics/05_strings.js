3751ed670230e2f315d3caf5292dfa699d436c7d
Golaji01 <ujjwalgola8077@gmail.com>
2025-06-27 21:52:17 +0530

    string

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,11 @@
+const name="John";
+const repoCount = 10;
+
+console.log(name + " has "+ repoCount + " repositories.");
+console.log(`Hello my name is ${name} and I have ${repoCount} repositories.`);
+
+const gameName = new String("Call of Duty");
+
+console.log(gameName[0]); 
+console.log(gameName.length);
+console.log(gameName._proto_);
